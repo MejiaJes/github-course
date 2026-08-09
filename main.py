07035096937478cda9ec751698e1ca9e524e6c04
MejiaJes <17jmejia89@gmail.com>
2026-08-08 23:42:51 -0400

    Revert "added a function"
    This reverts commit 74a521f26c1fe5eb261946bdd58f046419977fe8.
    
    reverted function

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 user_name = input('What is your name...')
 
 print(f"Your name {user_name} has {len(user_name)} characters!")
-
-# let's add a function
-if len(user_name) <= 5:
-    print("You have a short name")
-else:
-    print('You have a long name')
