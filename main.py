2444d25bc2f902ade59d247dc58432989fc953fd
MejiaJes <17jmejia89@gmail.com>
2026-08-09 12:07:08 -0400

    added math functions script

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from math_operators import add_numbers
+
 user_name = input('What is your name...')
 user_age = input("What is your age?: ")
 
@@ -8,3 +10,7 @@ if len(user_name) <= 5:
     print("Your name is short")
 else:
     print('You have a long name!')
+
+x = add_numbers(1, 2)
+
+print(x)
