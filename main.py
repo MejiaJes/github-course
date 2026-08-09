7c43f319e53198eadf213e5a6cd99480f6b4b653
MejiaJes <17jmejia89@gmail.com>
2026-08-09 00:27:48 -0400

    Added len name function to main branch

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 user_name = input('What is your name...')
 
 print(f"Your name {user_name} has {len(user_name)} characters!")
+
+if len(user_name) <= 5:
+    print("Your name is short")
+else:
+    print('You have a long name!')
