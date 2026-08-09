7a756314156c28adbf4071df01efb5e482efccd1
MejiaJes <17jmejia89@gmail.com>
2026-08-09 00:23:04 -0400

    added age input

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 user_name = input('What is your name...')
+user_age = input("What is your age?: ")
 
 print(f"Your name {user_name} has {len(user_name)} characters!")
+print(f"Your age is {user_age}")
